e6db33161c392da391fba0ebc3e1433e7e1b7374
castro kaiser <castro.nunes@aluno.senai.br>
2024-10-23 21:25:02 -0300

    atualizando exercicio 6, 7 e 8

--- exercicio_6/src/main/java/com/mycompany/exercicio_6/Exercicio_6.java
+++ exercicio_6/src/main/java/com/mycompany/exercicio_6/Exercicio_6.java
@@ -11,6 +11,15 @@ package com.mycompany.exercicio_6;
 public class Exercicio_6 {
 
     public static void main(String[] args) {
-        
+        int tempo = 10; // Definir o tempo inicial da contagem regressiva
+        for (int i = tempo; i >= 0; i--) {
+            System.out.println("Tempo restante: " + i);
+            try {
+                Thread.sleep(1000); // Esperar 1 segundo
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println("Processo finalizado!");
     }
 }
