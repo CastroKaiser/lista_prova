7ce4d74c2c626fe9579b2df723c3bbbd8af00592
castro kaiser <castro.nunes@aluno.senai.br>
2024-10-23 21:46:46 -0300

    Atualização incompleta do arquivo 10

--- exercicio_10/src/main/java/com/mycompany/exercicio_10/Exercicio_10.java
+++ exercicio_10/src/main/java/com/mycompany/exercicio_10/Exercicio_10.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.exercicio_10;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,7 +14,29 @@ import javax.swing.JOptionPane;
 public class Exercicio_10 {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "test");
-        JOptionPane.showMessageDialog(null, "test\nte");
+        Scanner ler = new Scanner(System.in);
+        int qtdParticipantes;
+        String generoAtual;
+        int contador = 0;
+        
+        System.out.print("Quantos participantes vao participar da pesquisa:");
+        qtdParticipantes = ler.nextInt();
+        
+        String[] genero = new String[qtdParticipantes];
+        
+        for (int i = 0; i < qtdParticipantes; i++) {
+            System.out.print("Qual gênero favorito do participante "+1+":");
+            genero[i] = ler.nextLine();
+        }
+        for (int i = 0; i < qtdParticipantes; i++) {
+            generoAtual = genero[i];
+            for (int j = 0; j < qtdParticipantes; j++) {
+                if (generoAtual.equals(genero[i])) {
+                    contador++;
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Existe "+contador+" participantes que gostam do genero "+generoAtual);
+        }
+    
     }
 }
